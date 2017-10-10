87ecd2fa04cff064344cef7fa1a3ee6a656dccd4
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-10-10 14:36:26 -0700

    Added tests and functionality for quadratic(). Tests various computations and exception handling for negative or undefined answers.

--- lab1.cpp
+++ lab1.cpp
@@ -21,7 +21,18 @@ unsigned int factorial( unsigned int number ) {
 }
 
 // Quadratic equation.
-double quadratic(int a, int b, int c);
+// While the quadratic equation has a plus or minus, only consider the plus case.
+double quadratic(int a, int b, int c) {
+    // discriminant is negative exception
+    if(pow(b, 2) - 4 * a * c < 0) throw "Discriminant is negative";
+
+    // answer is undefined exception
+    if(!a) throw "Answer is undefined";
+
+    // calculation
+    const int precision = 4; // decimal precision of answer
+    return round((-1 * b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a) * pow(10, precision)) / pow(10, precision);
+}
 
 
 // Greatest Common Divisor (GCD).
@@ -47,7 +58,7 @@ string dayOfTheWeek(int month, int day, int year);
 void parseStudentName(const string studentName, string& firstName, string& lastName, string& username);
 
 
-// Tests...
+// FACTORIAL TESTS
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial(1) == 1 );
     REQUIRE( factorial(2) == 2 );
@@ -55,3 +66,65 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial(10) == 3628800 );
 }
 
+// QUADRATIC TESTS
+TEST_CASE("Quadratic value is computed", "[quadratic]") {
+    // test values declaration
+    int tv[3];
+
+    SECTION("Discriminant is positive") {
+        // answer declaration
+        double answer;
+
+        SECTION("Evaluates to whole number") {
+            // set test values and answer
+            tv[0] = 1;
+            tv[1] = 2;
+            tv[2] = -3;
+            answer = 1.0;
+
+            // requirements
+            REQUIRE_NOTHROW(quadratic(tv[0], tv[1], tv[2]));
+            REQUIRE(quadratic(tv[0], tv[1], tv[2]) == answer);
+        }
+        SECTION("Evaluates to rational number with finite decimal places") {
+            // set test values and answer
+            tv[0] = 4;
+            tv[1] = 2;
+            tv[2] = -2;
+            answer = 0.5;
+
+            // requirements
+            REQUIRE_NOTHROW(quadratic(tv[0], tv[1], tv[2]));
+            REQUIRE(quadratic(tv[0], tv[1], tv[2]) == answer);
+        }
+        SECTION("Evaluates to rational number with infinite decimal places") {
+            // set test values and answer
+            tv[0] = 2;
+            tv[1] = 2;
+            tv[2] = -6;
+            answer = 1.3028;
+
+            // requirements
+            REQUIRE_NOTHROW(quadratic(tv[0], tv[1], tv[2]));
+            REQUIRE(quadratic(tv[0], tv[1], tv[2]) == answer);
+        }
+    }
+    SECTION("Discriminant is negative") {
+        // set test values
+        tv[0] = 1;
+        tv[1] = 2;
+        tv[2] = 3;
+
+        // requirements
+        REQUIRE_THROWS_WITH(quadratic(tv[0], tv[1], tv[2]), "Discriminant is negative");
+    }
+    SECTION("Answer is undefined") {
+        // set test values
+        tv[0] = 0; // a must be 0 to be undefined
+        tv[1] = 1;
+        tv[2] = 2;
+
+        // requirements
+        REQUIRE_THROWS_WITH(quadratic(tv[0], tv[1], tv[2]), "Answer is undefined");
+    }
+}
