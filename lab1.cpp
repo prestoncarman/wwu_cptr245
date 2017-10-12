4a6ea2203e11cbc1056d1c31899d16ba2eaeaa80
Ethan Beaver <ethan.beaver@ahss.org>
2017-10-11 23:09:28 -0700

    Initializing a variable

--- lab1.cpp
+++ lab1.cpp
@@ -38,7 +38,7 @@ double squareRoot(double value);
 string dayOfTheWeek(int month, int day, int year) {
     static int f[] = {0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
     year -= month < 3;
-    dayOfWeek = (year + year/4 - year/100 + year/400 + f[month-1] + day) % 7;
+    int dayOfWeek = (year + year/4 - year/100 + year/400 + f[month-1] + day) % 7;
     static string days[] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     return days[dayOfWeek];
 }
