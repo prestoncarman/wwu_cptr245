9411827dca025b946d7bc972e5fb82a1d6b696dc
sheldonwoodward <sheldon.k.woodward@gmail.com>
2017-10-11 20:33:22 -0700

    Added tests and functionality for squareRoot(). Tests various square root computations and exception handling for negative inputs.

--- lab1.cpp
+++ lab1.cpp
@@ -30,7 +30,22 @@ double gcd(int number1, int number2);
 
 // Babylonian Algorithm for square root.
 // Absolute C++ Ch3 PP14
-double squareRoot(double value);
+double squareRoot(double value) {
+    // bad value exception
+    if(value < 0) throw "Passed bad value";
+    if(value == 0) return 0.0;
+
+    // babylonian algorithm
+    double guess = value / 2;
+    double oldGuess;
+    double r;
+    while(guess - oldGuess != 0.0) {
+        oldGuess = guess;
+        r = value / guess;
+        guess = (guess + r) / 2;
+    }
+    return guess;
+}
 
 
 // Calculate what day of the week corresponds to the date.
@@ -55,3 +70,43 @@ TEST_CASE( "Factorials are computed", "[factorial]" ) {
     REQUIRE( factorial(10) == 3628800 );
 }
 
+TEST_CASE("Square roots are computed", "[squareRoot]") {
+    SECTION("Passed perfect square") {
+        // requirements
+        REQUIRE(squareRoot(4.0) == sqrt(4.0));
+        REQUIRE(squareRoot(9.0) == sqrt(9.0));
+        REQUIRE(squareRoot(16.0) == sqrt(16.0));
+        REQUIRE(squareRoot(25.0) == sqrt(25.0));
+        REQUIRE(squareRoot(36.0) == sqrt(36.0));
+        REQUIRE(squareRoot(49.0) == sqrt(49.0));
+        REQUIRE(squareRoot(64.0) == sqrt(64.0));
+        REQUIRE(squareRoot(81.0) == sqrt(81.0));
+        REQUIRE(squareRoot(100.0) == sqrt(100.0));
+    }
+    SECTION("Passed imperfect square") {
+        // TODO: Check for proper testing/TDD form
+        // test customizability
+        const double PREC = 10; // number of decimal places to require accuracy of. default: 10
+        const double INCR_X = 0.001; // x-value of y=x^3 where y is the val passed into squareRoot(). Default: 0.001
+        const int TEST_NUM = 10000; // number of tests to run. Default: 10,000
+        const double OFFSET = 0.0; // offset added to y to test different range of values in the same scope. Default: 0.0
+
+        // requirements
+        double roundPrec = pow(10, PREC);
+        double val;
+        for(double i = 0.0; i < INCR_X * TEST_NUM; i += INCR_X) {
+            val = pow(i, 3) + OFFSET; // val=i^3 for better curve of values to test
+
+            // require rounded values to be equal
+            REQUIRE(round(squareRoot(val) * roundPrec) / roundPrec == round(sqrt(val) * roundPrec) / roundPrec);
+        }
+    }
+    SECTION("Passed negative value") {
+        //requirements
+        REQUIRE_THROWS_WITH(squareRoot(-1.0), "Passed bad value");
+    }
+    SECTION("Passed zero") {
+        // requirements
+        REQUIRE(squareRoot(0.0) == 0.0);
+    }
+}
